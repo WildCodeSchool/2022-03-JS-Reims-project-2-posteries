9690a026f892d38ab0c7e55403e43b04def7d918
MORETTE Valentin <morette.valentin@gmail.com>
2022-04-08 14:30:26 +0200

    add Answer on Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,17 @@
 import Poster from "../components/Poster";
+import Answer from "../components/Answer";
 
 export default function Home() {
-  return <Poster />;
+  <>
+    <p>Timer</p>
+    <p>Points</p>
+    <h1>Posteries</h1>
+    <Poster />
+    <Answer name="Le Bon, La Brute, Le Truand" />
+    <Answer name="Mary à tout prix" />
+    <Answer name="Les dix commandements" />
+    <Answer name="Ponyo sur la falaise" />
+    <p>Setting</p>
+    <p>Hint</p>
+  </>;
 }
