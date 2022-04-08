e950245fc1df0ec3830ebcb43b8dd1da6a1c80ab
MORETTE Valentin <morette.valentin@gmail.com>
2022-04-08 10:06:28 +0200

    add p and Answer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,17 @@
+import Answer from "../components/Answer";
 import Poster from "../components/Poster";
 
 export default function Home() {
   return (
     <>
+      <p>Timer</p>
+      <p>Points</p>
       <h1>Posteries</h1>
       <Poster />
+      <Answer name="Le Bon, La Brute, Le Truand" />
+      <Answer name="Le Bon, La Brute, Le Truand" />
+      <Answer name="Le Bon, La Brute, Le Truand" />
+      <Answer name="Le Bon, La Brute, Le Truand" />
     </>
   );
 }
