a6bf83a705c12141361dd49268734c566c889116
MORETTE Valentin <morette.valentin@gmail.com>
2022-04-08 14:56:14 +0200

    import Answer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,18 @@ import Poster from "../components/Poster";
 import Answer from "../components/Answer";
 
 export default function Home() {
-  <>
-    <p>Timer</p>
-    <p>Points</p>
-    <h1>Posteries</h1>
-    <Poster />
-    <Answer name="Le Bon, La Brute, Le Truand" />
-    <Answer name="Mary à tout prix" />
-    <Answer name="Les dix commandements" />
-    <Answer name="Ponyo sur la falaise" />
-    <p>Setting</p>
-    <p>Hint</p>
-  </>;
+  return (
+    <>
+      <p>Timer</p>
+      <p>Points</p>
+      <h1>Posteries</h1>
+      <Poster />
+      <Answer name="Le Bon, La Brute, Le Truand" />
+      <Answer name="Mary à tout prix" />
+      <Answer name="Les dix commandements" />
+      <Answer name="Ponyo sur la falaise" />
+      <p>Setting</p>
+      <p>Hint</p>
+    </>
+  );
 }
