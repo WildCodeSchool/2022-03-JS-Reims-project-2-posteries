29dfc9046b34149d58e783c276e37fe199c62288
Simon Duc <ducsimon@gmail.com>
2022-04-14 16:56:11 +0200

    moved the movieIdArray and randomMovie variables in the getMovie function

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,15 +6,15 @@ import Poster from "../components/Poster";
 export default function Home() {
   const [movie, setMovie] = useState([]);
 
-  const movieIdArray = [
-    11, 19, 105, 120, 129, 272, 329, 406, 429, 500, 524, 550, 1878, 22538,
-    27205, 37799, 157336, 313369, 333339, 419430,
-  ];
-
-  const randomMovie =
-    movieIdArray[Math.floor(Math.random() * movieIdArray.length)];
-
   function getMovie() {
+    const movieIdArray = [
+      11, 19, 105, 120, 129, 272, 329, 406, 429, 500, 524, 550, 1878, 22538,
+      27205, 37799, 157336, 313369, 333339, 419430,
+    ];
+
+    const randomMovie =
+      movieIdArray[Math.floor(Math.random() * movieIdArray.length)];
+
     axios
       .get(
         `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=df8d2d90ff4e6f4a0f1e460dda3a4a35`
