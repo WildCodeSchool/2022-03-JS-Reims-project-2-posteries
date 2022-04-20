a8f2e7b71ba898db9fff49e4e1ad3cf7c8d218c4
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-04-20 16:05:52 +0200

    add falseMovie and button next question

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,26 +4,41 @@ import Answer from "../components/Answer";
 import Poster from "../components/Poster";
 
 export default function Home() {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState();
+  const [falseMovie, setFalseMovie] = useState();
+  const movieIdArray = [
+    11, 19, 105, 120, 129, 272, 329, 406, 429, 500, 524, 550, 1878, 22538,
+    27205, 37799, 157336, 313369, 333339, 419430,
+  ];
+
+  const [movieId, setMovieId] = useState(
+    movieIdArray[Math.floor(Math.random() * movieIdArray.length)]
+  );
+
+  function pickRandomMovieId() {
+    setMovieId(movieIdArray[Math.floor(Math.random() * movieIdArray.length)]);
+  }
 
   function getMovie() {
-    const movieIdArray = [
-      11, 19, 105, 120, 129, 272, 329, 406, 429, 500, 524, 550, 1878, 22538,
-      27205, 37799, 157336, 313369, 333339, 419430,
-    ];
-
-    const randomMovie =
-      movieIdArray[Math.floor(Math.random() * movieIdArray.length)];
-
     axios
       .get(
-        `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=df8d2d90ff4e6f4a0f1e460dda3a4a35`
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=df8d2d90ff4e6f4a0f1e460dda3a4a35`
       )
       .then((resp) => resp.data)
       .then((data) => setMovie(data));
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=df8d2d90ff4e6f4a0f1e460dda3a4a35`
+      )
+      .then((resp) => resp.data)
+      .then((data) => setFalseMovie(data));
   }
 
-  useEffect(getMovie, []);
+  function nextLevel() {
+    pickRandomMovieId();
+  }
+
+  useEffect(getMovie, [movieId]);
 
   return (
     <>
@@ -32,13 +47,32 @@ export default function Home() {
         <p>Points</p>
       </div>
       <h1>Posteries</h1>
-      <Poster poster={movie.poster_path} title={movie.original_title} />
-      <div className="answers">
-        <Answer title={movie.original_title} movie={movie} res />
-        <Answer title="Mary à tout prix" movie={movie} res={false} />
-        <Answer title="Les dix commandements" movie={movie} res={false} />
-        <Answer title="Ponyo sur la falaise" movie={movie} res={false} />
-      </div>
+      {movie && falseMovie && (
+        <>
+          <Poster poster={movie.poster_path} title={movie.original_title} />
+          <div className="answers">
+            <Answer title={movie.original_title} movie={movie} res />
+            <Answer
+              title={falseMovie.original_title}
+              movie={movie}
+              res={false}
+            />
+            <Answer
+              title={falseMovie.original_title}
+              movie={movie}
+              res={false}
+            />
+            <Answer
+              title={falseMovie.original_title}
+              movie={movie}
+              res={false}
+            />
+          </div>
+        </>
+      )}
+      <button type="button" onClick={nextLevel}>
+        NEXT QUESTION
+      </button>
     </>
   );
 }
