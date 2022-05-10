d7eb918063b318cf948a46a3fbfefcb8dcd92151
MORETTE Valentin <morette.valentin@gmail.com>
2022-05-10 11:53:28 +0200

    pickmovie is pure function now

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -46,7 +46,7 @@ export default function Play() {
   };
 
   function nextLevel() {
-    pickMovie(movieIdArray);
+    pickMovie(movieIdArray, (count + 1) * 4);
     setCount(count + 1);
     reset();
     start();
@@ -55,7 +55,7 @@ export default function Play() {
 
   useEffect(() => {
     movieIdArray.sort(() => Math.random() - 0.5);
-    pickMovie(movieIdArray);
+    pickMovie(movieIdArray, count * 4);
   }, []);
 
   return (
