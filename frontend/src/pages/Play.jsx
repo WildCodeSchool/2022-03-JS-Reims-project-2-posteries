cb3f6abf20cd2111d7afcac14e80c4bcda4ed615
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-05-11 09:21:39 +0200

    linted the modifications brought to the time-dependant answer reveal

--- frontend/src/pages/Play.jsx
+++ frontend/src/pages/Play.jsx
@@ -70,10 +70,14 @@ export default function Play() {
           <div className="frame">
             <button
               type="button"
-              onClick={isAnswerActive ? nextLevel : null}
-              className={isAnswerActive ? "now-showing next" : "now-showing"}
+              onClick={isAnswerActive || time === 0 ? nextLevel : null}
+              className={
+                isAnswerActive || time === 0
+                  ? "now-showing next"
+                  : "now-showing"
+              }
             >
-              {isAnswerActive ? "NEXT" : "NOW SHOWING"}
+              {isAnswerActive || time === 0 ? "NEXT" : "NOW SHOWING"}
             </button>
             <Poster
               poster={movie.poster_path}
@@ -84,7 +88,7 @@ export default function Play() {
           <div className="answers">
             <AnswerList
               answersArray={movie.answers}
-              isAnswerActive={isAnswerActive}
+              isAnswerActive={isAnswerActive || time === 0}
               setIsAnswerActive={setIsAnswerActive}
               activateAnswer={activateAnswer}
             />
